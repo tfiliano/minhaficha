2403515a763567c81600fb1b6f4d543fec869872
Thiago Figliano Rosa <figlianorosathiago@gmail.com>
2024-08-25 23:48:04 +0100

    comecando corpo

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,57 @@
+import { AnimationTransitionPage } from "@/components/animation";
+import { CardButton, ContentGrid, Title } from "@/components/layout";
+
+export default function Admin() {
+  const menu = [
+    {
+      title: "Produtos",
+      route: "/admin/produtos",
+    },
+    {
+      title: "Etiquetas",
+      route: "/adm-etiquetas",
+    },
+    {
+      title: "Grupos",
+      route: "/adm-grupos",
+    },
+    {
+      title: "Armazenamento",
+      route: "/adm-armazenamento",
+    },
+    {
+      title: "Operadores",
+      route: "/adm-operadores",
+    },
+    {
+      title: "Fabricantes",
+      route: "/adm-fabricantes",
+    },
+    {
+      title: "Impressoras",
+      route: "/adm-impressoras",
+    },
+    {
+      title: "Configuraçõe da Loja",
+      route: "/adm-configuraçõe da Loja",
+    },
+  ];
+
+  return (
+    <AnimationTransitionPage>
+      {/* <Title>SELECIONE UMA OPERAÇÃO</Title> */}
+      <ContentGrid>
+        {menu.map((item) => (
+          <CardButton
+            key={item.title}
+            title={item.title}
+            url={{
+            pathname: item.route ,
+              query: { item: item.title },
+            }}
+          />
+        ))}
+      </ContentGrid>
+    </AnimationTransitionPage>
+  );
+}
